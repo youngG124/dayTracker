440df05672f72c74bc8b304dd02533212d191d8f
newyoung124 <newyoung124@naver.com>
2024-12-11 20:24:35 +0900

    getter & setter

--- src/main/java/com/desciplineGrasser/demo/User.java
+++ src/main/java/com/desciplineGrasser/demo/User.java
@@ -10,4 +10,19 @@ public class User {
     private String email;
 
     // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
